0d9bcaa3288fbd07d500783208d193cc04774221
Yahor \|/ <yagorpraca@gmail.com>
2023-12-19 14:40:43 +0300

    Task2

---
+++ src/Task2/Test.java
@@ -0,0 +1,14 @@
+package Task2;
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            System.out.println("В блоке try");
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("В блоке catch");
+        } finally {
+            System.out.println("В блоке finally");
+        }
+    }
+}
